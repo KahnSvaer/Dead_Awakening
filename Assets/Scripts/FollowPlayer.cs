4b4d79ef24cd36c6b4a1c33a83fa9ef2506a603b
Shivansh Pachnanda <Shivansh.pachnanda1@gmail.com>
2024-02-05 09:46:53 +0530

    Added Sandbox level, Targetting Enemy prefab

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
