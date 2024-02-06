dfe0c040616f727c1d9d7ddf0bf8b4a88fe8da95
Shivansh Pachnanda <Shivansh.pachnanda1@gmail.com>
2024-02-06 12:59:57 +0530

    Added Enemy AI, Imported Guns Asset

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,19 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5; 
+    
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+
+    bool isProvoked;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        distanceToTarget = Vector3.Distance(transform.position,target.position);
     }
 
     void Update()
     {
+        FollowTarget();
+    }
+
+
+    private void FollowTarget()
+    {
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if(isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance){
+        ChaseTarget();
+        }
+        if(distanceToTarget <= navMeshAgent.stoppingDistance){
+            AttackTarget();
+        }
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("Attacking Target ");
+    }
+
+    void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
+    }
+
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,chaseRange);
     }
 }
